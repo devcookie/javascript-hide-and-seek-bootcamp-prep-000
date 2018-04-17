b4dfa375803baa72e25e2daf6dda2ef7d1f915dc
Catherine Lee <lee.catherine77@gmail.com>
2018-04-17 20:00:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function deepestChild() {
   var begin = document.getElementById('grand-node')
   var store = []
   for(var i = 0; i < begin.length; i++) {
-    store = begin[i].querySelector('div').innerHTML
+    store = begin[i].innerHTML
   }
   return store
 }
