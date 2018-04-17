e4e20c4ec959a4fb975e7f8f12ff20e69775b54a
Catherine Lee <lee.catherine77@gmail.com>
2018-04-17 13:59:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  //Select elements
+  var ranked = document.getElementById('app').querySelectorAll('.ranked-list li')
+  for(var i = 0; i < ranked.length; i++) {
+    //Iterate through elements to grab their innerHTML, add int values
+    ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + parseInt(n)
+  }
+}
+
+function deepestChild() {
+  var begin = document.getElementById('app').querySelector('div#grand-node')
+  for(var i = 0; i < begin.length; i++) {
+    
+  }
+}
